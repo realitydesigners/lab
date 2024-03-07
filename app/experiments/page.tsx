43e70f09c95de0d52ef6a1d16d4e5927888ff384
RD <raymond.luke.spartz@gmail.com>
2024-03-07 13:27:05 -0800

    formatted experiment page

--- app/experiments/page.tsx
+++ app/experiments/page.tsx
@@ -1,21 +1,19 @@
 import ExperimentsList from "@/components/ExperimentList";
-
 import { sanityFetch } from "@/sanity/lib/client";
 import { experimentQuery } from "@/sanity/lib/queries";
-
 import { ExperimentsPayload } from "@/types";
 
 export default async function PostsPage() {
-	const experiments: ExperimentsPayload[] = await sanityFetch({
-		query: experimentQuery,
-		tags: ["posts"],
-	});
+    const experiments: ExperimentsPayload[] = await sanityFetch({
+        query: experimentQuery,
+        tags: ["posts"],
+    });
 
-	const postsListData = experiments.slice(0, 30);
+    const postsListData = experiments.slice(0, 30);
 
-	return (
-		<main className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
-			<ExperimentsList post={postsListData} />
-		</main>
-	);
+    return (
+        <main className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+            <ExperimentsList post={postsListData} />
+        </main>
+    );
 }
